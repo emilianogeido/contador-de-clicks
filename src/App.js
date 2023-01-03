0a2bfae0fe56e955164fa19fef5a531c58dc7cd4
emilianogeido <110432365+emilianogeido@users.noreply.github.com>
2023-01-03 12:18:57 -0300

    Implemented Boton and Contador

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+import Boton from './componentes/Boton'
+import Contador from './componentes/Contador';
+import {useState} from 'react';
+
 function App() {
+
+const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+   setNumClicks (numClicks + 1);
+  }
+
+  const reiniciarContador = () => { 
+    setNumClicks (0);
+  }
+  
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
@@ -10,7 +25,16 @@ function App() {
         alt='Logo de freeCodeCamp'/>  
       </div>
       <div className='contenedor-contador'>
-
+        <Contador numclicks={numClicks}/>
+        <Boton
+        texto = 'Click'
+        esBotonDeClick = {true}
+        manejarClick = {manejarClick} />
+        <Boton 
+        texto = 'Reiniciar'
+        esBotonDeClick = {false}
+        manejarClick = {reiniciarContador} />
+        
       </div>
     </div>
   );
--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../hojas-de-estilo/Boton.css'
 
-function Boton({texto, esBotonDeClick}) {
+
+// "texto" es el texto que quiero que diga el boton, esBotonDeClick es un operador ternario 
+// y manejarClick es una funcion
+function Boton({texto, esBotonDeClick, manejarClick}) {
     return(
-        <button className={esBotonDeClick ? 'boton-click' : 'boton-reiniciar'}>
+        <button className={esBotonDeClick ? 'boton-click' : 'boton-reiniciar'}
+            onClick={manejarClick} >
             {texto}
         </button>
     )
 }
+
+export default Boton;
